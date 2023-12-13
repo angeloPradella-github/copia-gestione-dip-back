00285ff03711fe07432a3fa46de37039946a37a6
angeloPradella-github <angelogpradella@gmail.com>
2023-12-13 15:38:26 +0100

    aggiunta delle properties forse corrette

--- create.sql
+++ create.sql
@@ -113,3 +113,49 @@
        add constraint FK3wxdofviqe2smmvh1w1yf98o1 
        foreign key (user_id) 
        references user (id);
+
+    create table log (
+        id integer not null auto_increment,
+        user_id integer not null,
+        entry_time datetime(6),
+        exit_time datetime(6),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table user (
+        id integer not null auto_increment,
+        email varchar(255),
+        name varchar(255),
+        password varchar(255),
+        role varchar(255),
+        surname varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
+
+    alter table log 
+       add constraint FK3wxdofviqe2smmvh1w1yf98o1 
+       foreign key (user_id) 
+       references user (id);
+
+    create table log (
+        id integer not null auto_increment,
+        user_id integer not null,
+        entry_time datetime(6),
+        exit_time datetime(6),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table user (
+        id integer not null auto_increment,
+        email varchar(255),
+        name varchar(255),
+        password varchar(255),
+        role varchar(255),
+        surname varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
+
+    alter table log 
+       add constraint FK3wxdofviqe2smmvh1w1yf98o1 
+       foreign key (user_id) 
+       references user (id);
--- drop.sql
+++ drop.sql
@@ -38,3 +38,19 @@
     drop table if exists log;
 
     drop table if exists user;
+
+    alter table log 
+       drop 
+       foreign key FK3wxdofviqe2smmvh1w1yf98o1;
+
+    drop table if exists log;
+
+    drop table if exists user;
+
+    alter table log 
+       drop 
+       foreign key FK3wxdofviqe2smmvh1w1yf98o1;
+
+    drop table if exists log;
+
+    drop table if exists user;
